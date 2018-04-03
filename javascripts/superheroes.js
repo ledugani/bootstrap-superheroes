b169b53db99b5d52fe9bc5ba0c6c167f8617919a
Tom Dugan <thomas@aadwebb.com>
2018-04-03 18:03:42 -0500

    populate superheroes page

--- javascripts/superheroes.js
+++ javascripts/superheroes.js
@@ -1,1 +1,35 @@
-console.log();
+const printToDom = (domString, divId) => {
+    document.getElementById(divId).innerHTML = domString;
+};
+
+const buildDomString = fancyArray => {
+    let domString = "";
+    fancyArray.forEach((hero) => {
+        domString += `<div class="col-md-4">`;
+        domString += `<img src="${hero.image}">`;
+        domString += `<h2>${hero.name}</h2>`;
+        domString += `<p><em>${hero.gender}</em></p>`;
+        domString += `<p>${hero.description}</p>`;
+        domString += `</div>`;
+    });
+    printToDom(domString, 'card-holder');
+}
+
+function executeThisCodeIfXHRFails () {
+    console.log("something broke")
+}
+
+function executeThisCodeAfterFileLoaded () {
+    const data = JSON.parse(this.responseText);
+    buildDomString(data.superheroes);
+}
+
+const startApplication = () => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+    myRequest.addEventListener("error", executeThisCodeIfXHRFails);
+    myRequest.open("GET", "/db/superheroes.json");
+    myRequest.send();
+}
+
+startApplication();
